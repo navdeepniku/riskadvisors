b65b94abbf20a636c64ed6b6c970e129d1b16dd6
navdeep <navdeepniku@gmail.com>
2016-08-10 02:12:46 +0530

    test

--- riskadvisors/routes.py
+++ riskadvisors/routes.py
@@ -1,9 +1,11 @@
 from flask import url_for
 from flask import request, redirect
 
+import os
+
 from riskadvisors import app
 
 @app.route('/')
 def home():
     redirect(url_for('home'))
-    return "hi"+$Database
+    return "hi"+os.environ["Database"]
