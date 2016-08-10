42f59cfbe71acf0350866b840d974ffdb2caab8b
navdeep <navdeepniku@gmail.com>
2016-08-10 11:54:31 +0530

    test

--- riskadvisors/__init__.py
+++ riskadvisors/__init__.py
@@ -16,7 +16,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
 
     def __init__(self, email):
-    self.email = email
+        self.email = email
 
     def __repr__(self):
         return '<E-mail %r>' % self.email
