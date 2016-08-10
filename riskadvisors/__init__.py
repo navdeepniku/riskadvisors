813c4d7b64f2718f207fa2617fbdbdd5f463c6f1
navdeep <navdeepniku@gmail.com>
2016-08-10 19:25:58 +0530

    test

--- riskadvisors/__init__.py
+++ riskadvisors/__init__.py
@@ -63,8 +63,9 @@ def upload_file():
 @app.route('/test')
 def test():
     import wget
-    file = wget.download("https://www.dropbox.com/s/zt1xyzqhqfdqxr0/Stock%20Data.xlsx?dl=1")
-    return redirect(url_for('after_upload', filename = file))
+    #file = wget.download("https://www.dropbox.com/s/zt1xyzqhqfdqxr0/Stock%20Data.xlsx?dl=1")
+    #return redirect(url_for('after_upload', filename = file))
+    return os.system("pwd")
 
 @app.route('/after_upload/<filename>')
 def after_upload(filename):
