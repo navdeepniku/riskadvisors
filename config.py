c338e8307ff8c485276b4c34c5edb800e788516f
navdeep <navdeepniku@gmail.com>
2016-08-10 11:51:34 +0530

    test

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URI = os.environ['DATABASE_URL']
--- riskadvisors/__init__.py
+++ riskadvisors/__init__.py
@@ -1,10 +1,28 @@
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import DATABASE_URI
 
 #create application object
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URI
+db = SQLAlchemy(app)
 
+#create db model
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(120), unique=True)
 
+    def __init__(self, email):
+    self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+db.create_all()
 
 from riskadvisors import routes
+
+
