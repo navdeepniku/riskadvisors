bd53486646fa087c95653707e6e3f8260d67198a
navdeep <navdeepniku@gmail.com>
2016-08-11 01:19:21 +0530

    test

--- riskadvisors/__init__.py
+++ riskadvisors/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask import request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -11,9 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URI
 db = SQLAlchemy(app)
 
 #create db model
-
-class User(db.Model):
-    __tablename__ = "users"
+'''
+class Stock(db.Model):
+    __tablename__ = ""
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique=True)
 
@@ -24,7 +23,7 @@ class User(db.Model):
         return '<E-mail %r>' % self.email
 
 db.create_all()
-
+'''
 
 @app.route('/')
 def home():
@@ -38,7 +37,6 @@ def home():
     for u in us:
         email=u.email
     return "hi"+os.environ["Database"]+" email for u1 :"+email
-
 #from riskadvisors import routes
 
 #upload_folder = 'C://Users/navdeep/Documents/Github/riskadvisors/tmp/'
@@ -52,26 +50,40 @@ def upload_file():
         filename  = file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return redirect(url_for('after_upload', filename = filename))
+    
+    if request.method == 'POST' and  request.form['file_url'] != 'file_url':
+        file_url = request.form['file_url']
+        file_url = file_url[0:-1]+'1'
+        return redirect(url_for('dropbox_handle', file_url=file_url))
+
     return '''
         <!doctype html>
         <h1>Upload Xlsx file</h1>
         <form action="" method=post enctype=multipart/form-data>
-            <p><input type=file name=file>
+            <p><input type=file name=file></p>
+            <p>OR</p>
+            <p> Enter Dropbox file url </p>
+            <p><input type=text name=file_url value=file_url> 
                <input type=submit value=Upload>
         </form>
         '''
-@app.route('/test')
-def test():
-    import wget
-    filename = "f1.xlsx"
-    f = wget.download("https://www.dropbox.com/s/zt1xyzqhqfdqxr0/Stock%20Data.xlsx?dl=1",os.path.join(app.config['UPLOAD_FOLDER'],filename))
-    return redirect(url_for('after_upload', filename = filename))
-
+@app.route('/dropbox_handle/<file_url>')
+def dropbox_handle(file_url):
+    try:
+        import wget
+        filename = "f1.xlsx"
+        f = wget.download(file_url,os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        return redirect(url_for('after_upload', filename = filename))
+    except:
+        return "Enter valid file url"
 @app.route('/after_upload/<filename>')
 def after_upload(filename):
-    from openpyxl import load_workbook
-    wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],filename), read_only=True)
-    ws = wb.active
-    for row in ws.rows:
-        for cell in row:
-            return (cell.value)
+    try:
+        from openpyxl import load_workbook
+        wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],filename), read_only=True)
+        ws = wb.active
+        for row in ws.rows:
+            for cell in row:
+                return (cell.value)
+    except:
+        return "not a valid file"
