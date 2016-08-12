a711e1ee4ca8e126978b8b3d9a3b9eb607f73b8c
navdeep <navdeepniku@gmail.com>
2016-08-13 02:32:41 +0530

    test

--- riskadvisors/routes.py
+++ riskadvisors/routes.py
@@ -73,8 +73,8 @@ class sheet(object):
 @app.route('/after_upload/<filename>')
 def after_upload(filename):
         from openpyxl import load_workbook
-        #wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],filename), read_only=True)
-        wb = load_workbook(filename='C://users/navdeep/Desktop/book.xlsx', read_only=True)
+        wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],filename), read_only=True)
+        #wb = load_workbook(filename='C://users/navdeep/Desktop/book.xlsx', read_only=True)
         
         ws = wb.active
 
@@ -108,10 +108,10 @@ def db_model():
 @app.route('/db_commit')
 def db_commit():
     from openpyxl import load_workbook
-    #wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],session['filename']), read_only=True)
+    wb = load_workbook(filename=os.path.join(app.config['UPLOAD_FOLDER'],session['filename']), read_only=True)
     sheet_headers=session['sheet_headers']
     
-    wb = load_workbook(filename='C://users/navdeep/Desktop/book.xlsx', read_only=True)
+    #wb = load_workbook(filename='C://users/navdeep/Desktop/book.xlsx', read_only=True)
     ws = wb.active
         
         
