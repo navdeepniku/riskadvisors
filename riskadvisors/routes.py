699bd6122bf6eec0e8ecea73640bd197ee63191b
navdeep <navdeepniku@gmail.com>
2016-08-12 19:08:53 +0530

    test

--- riskadvisors/routes.py
+++ riskadvisors/routes.py
@@ -74,37 +74,39 @@ def after_upload(filename):
                 sheet_headers.append(cell.value)
             break
         
+        return redirect(url_for('db_model'),sheet_headers=sheet_headers)
+        
+        
+@app.route('/db_model')
+def db_model():
+        sheet_headers = request.args.get('sheet_headers')
         e=create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
         metadata = MetaData(bind=e)
         t = Table('sheet', metadata, Column('id', Integer, primary_key=True),*(Column(header, String(8000)) for header in sheet_headers))
         metadata.create_all()
         clear_mappers() 
         mapper(sheet, t)
-        db_session = create_session(bind=e, autocommit=False, autoflush=False)
+        return redirect(url_for('/db_commit'), db_session = create_session(bind=e, autocommit=False, autoflush=False))
         
-        count=0  
-        for r in ws.rows:
-            if count>0:
-                s = sheet()
-                cou=0
-                for c in r:
-                    setattr(s,sheet_headers[cou],c.value)
-                    cou+=1
-                db_session.add(s)
-                #if count%100==0:
-                #    print 'yes upload'
-                #    db_session.commit()
-                
-            count+=1
-
-        #db_session.commit()
-        
-        #return 'done'
-        return redirect(url_for('db_commit'), db_session=db_session)
 
 @app.route('/db_commit')
 def db_commit():
     db_session = request.args.get('db_session')
 
+    count=0  
+    for r in ws.rows:
+        if count>0:
+            s = sheet()
+            cou=0
+            for c in r:
+                setattr(s,sheet_headers[cou],c.value)
+                cou+=1
+            db_session.add(s)
+                #if count%100==0:
+                #    print 'yes upload'
+                #    db_session.commit()
+            count+=1
+
+        
     db_session.commit()
     return "done"
