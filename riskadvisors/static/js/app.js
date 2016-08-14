0a20aa2e0d9f903c81056a1610f5440c11857092
navdeep <navdeepniku@gmail.com>
2016-08-14 16:51:57 +0530

    test

--- riskadvisors/static/js/app.js
+++ riskadvisors/static/js/app.js
@@ -1,15 +1,17 @@
 (function(){
     var app = angular.module('angularJsStub', [ ]);
+    var querypass = {1:'1',id:'id'};
     app.controller('QueryData', function($scope,$http){
         $http({
             url: '/queryDb',
             method: "POST",
-            headers: { ' Content-Type': 'application/json'},
-            data: JSON.stringify(querypass)
-        }).success(function(data)){
-            $scope.datalist = data;
-        }
-    
+            data: querypass
+        }).then(function mySuccess(response) {
+            $scope.datalist = response.data;
+        }, function myError(response){
+            $scope.datalist = response.statusText;
         });
+
+    });
 
 })();
