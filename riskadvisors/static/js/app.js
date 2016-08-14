311094d8c7124d345205d160c6d15022c3834ca6
navdeep <navdeepniku@gmail.com>
2016-08-15 01:34:04 +0530

    test

--- riskadvisors/static/js/app.js
+++ riskadvisors/static/js/app.js
@@ -1,14 +1,9 @@
 (function(){
     var app = angular.module('angularJsStub', [ ]);
     var querypass = {col_value:'1',col_name:'id'};
-    app.controller('QueryData',[function($scope,$http){
+    app.controller('QueryData',function($scope,$http){
 
-        $scope.query = function () {
-            var qdata = {
-                col_name: $scope.col_name,
-                col_value: $scope.col_value
-            };
-
+        $scope.query = function ($qdata) {
             $http({
             url: '/queryDb',
             method: "POST",
@@ -20,6 +15,6 @@
         });
         }
 
-        }]);
+        });
 
 })();
