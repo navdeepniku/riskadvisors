d87a9713974901422c8779a33532d0762226c53a
navdeep <navdeepniku@gmail.com>
2016-08-14 13:43:35 +0530

    test

--- riskadvisors/static/js/app.js
+++ riskadvisors/static/js/app.js
@@ -1,16 +1,13 @@
 (function(){
     var app = angular.module('angularJsStub', [ ]);
 
-    var phone={ram: '512', cpu: 1};
     app.controller('QueryData', function($scope,$http){
-        var done=function(resp){
-            $scope.lists=respdata;
-        };
-        var fail=function(err){
-            
-        };
-        $http.get( {{ url_for('queryDb') }} )
-        .then(done,fail)
+        $http.get( {{ url_for('queryDb') }} ).then(function mySucces(response) {
+            $scope.datalist = response.data;
+        }, function myError(response){
+            $scope.datalist = response.statusText;
+        });
+
     });
 
 })();
